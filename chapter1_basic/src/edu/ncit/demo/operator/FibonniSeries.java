63c93935bbd9c60036908e41fc0e6934365591c4
Ram Gautam <ram.gautam@codapayments.com>
2023-12-12 19:24:57 +0545

    correct Fibonacci Series

--- chapter1_basic/src/edu/ncit/demo/operator/FibonniSeries.java
+++ chapter1_basic/src/edu/ncit/demo/operator/FibonniSeries.java
@@ -4,20 +4,8 @@ public class FibonniSeries {
     public static void main(String args[]) {
         int count = 10;
         int counter=1;
-        //System.out.println("series"+ fib(count));
         displayFibooniSeries(count);
-//        while (counter <= count) {
-//            System.out.println(fib(counter));
-//            count++;
-//        }
     }
-    static int fib(int n)
-    {
-        if (n <= 1)
-            return n;
-        return fib(n-1) + fib(n-2);
-    }
-
     public static void displayFibooniSeries(int count) {
         int firstTerm = 0, secondTerm = 1;
         System.out.println("Fibonacci Series till " + count + " terms:");
