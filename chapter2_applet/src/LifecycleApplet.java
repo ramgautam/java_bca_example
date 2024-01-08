2e4dc4fdb57d7e846bf8a76ac76d2d59a374459e
Ram Gautam <ram.gautam@codapayments.com>
2024-01-08 21:45:48 +0545

    add with different color text

--- chapter2_applet/src/LifecycleApplet.java
+++ chapter2_applet/src/LifecycleApplet.java
@@ -3,8 +3,31 @@ import java.awt.*;
 
 public class LifecycleApplet extends Applet {
 
+    private Color backgroundColor;
+
     public void init() {
         System.out.println("Init method called");
+
+        // Retrieve the parameter named "bgColor" from the HTML code
+        String colorParam = getParameter("bgColor");
+        String message = getParameter("message");
+
+        System.out.println("background color from param:"+colorParam);
+        System.out.println("message:"+message);
+
+        // If the parameter exists and represents a valid color, set the background color
+        if (colorParam != null) {
+            try {
+                backgroundColor = Color.decode(colorParam);
+                setBackground(backgroundColor);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid color value: " + colorParam);
+            }
+        } else {
+            // Default background color if parameter not provided or invalid
+            backgroundColor = Color.WHITE;
+            setBackground(backgroundColor);
+        }
         // Initialization code goes here
     }
 
@@ -25,6 +48,9 @@ public class LifecycleApplet extends Applet {
 
     public void paint(Graphics g) {
         g.drawString("Lifecycle Applet", 20, 20);
+        // Display the color value on the applet window
+        g.setColor(Color.RED);
+        g.drawString("Background color: " + backgroundColor.toString(), 30, 30);
         // Drawing or displaying content code goes here
     }
 }
