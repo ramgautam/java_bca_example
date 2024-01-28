6f83383c5370d22ab1d754a32d9d098bf3aec932
ramgautam <ramgau@gmail.com>
2024-01-28 11:12:28 +0545

    add label on gui program

--- Chaper5_GUI/src/BasicGUIProgram.java
+++ Chaper5_GUI/src/BasicGUIProgram.java
@@ -1,15 +1,19 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class BasicGUIProgram {
     private JFrame frame;
     private JButton button;
+    private JLabel label;
 
     public BasicGUIProgram() {
         frame = new JFrame("Basic GUI Program"); // Create a new JFrame
         frame.pack();
         button = new JButton("Click Me"); // Create a new JButton
+
+        label = new JLabel("test label");
+
+
+        label.setBounds(30, 50, 100, 10);
 
         // Set the size and layout of the frame
         frame.setSize(300, 200);
@@ -18,7 +22,9 @@ public class BasicGUIProgram {
 
         // Set the size and position of the button
         button.setBounds(100, 50, 100, 30);
+        button.setToolTipText("test tool tips text");
 
+        frame.add(label);
         // Add the button to the frame
         frame.add(button);
 
