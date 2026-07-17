76a9fb13780867aa6718b5de4fddb7e415a71c0a
ram-gautam <ramgautam@vivekahealth.com>
2026-07-17 08:38:40 +0545

    remove try with resource

--- chapter4_IO/src/FileInputOutputStreamDemo.java
+++ chapter4_IO/src/FileInputOutputStreamDemo.java
@@ -4,19 +4,25 @@ import java.io.IOException;
 
 public class FileInputOutputStreamDemo {
     public static void main(String[] args) {
+
+        //this is the file location
         String sourceFile = "/home/ram/source.txt";
         String destinationFile = "/home/ram/destination.txt";
 
-        try (
-                FileInputStream inputStream = new FileInputStream(sourceFile);
-                FileOutputStream outputStream = new FileOutputStream(destinationFile)
-        ) {
+        try{
+            //opening the stream based on type
+                FileInputStream inputStream = new FileInputStream(sourceFile);//
+                FileOutputStream outputStream = new FileOutputStream(destinationFile);//
+
             int byteData;
-            while ((byteData = inputStream.read()) != -1) {
-                outputStream.write(byteData);
+            while ((byteData = inputStream.read()) != -1) {// reading the content from the source file
+                outputStream.write(byteData);// writing the content into the destination file
             }
 
             System.out.println("File copied successfully!");
+            //Closing the stream
+            inputStream.close();
+            outputStream.close();
 
         } catch (IOException e) {
             System.out.println("Error during file operation: " + e.getMessage());
